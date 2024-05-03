74de74a7f68e7e43a191d4272d7e374d33611176
Anita Mishra <anitahmishra@gmail.com>
2024-05-03 21:42:18 +0530

    complex state object with props

--- react-practice-for-exercise/src/components/star.jsx
+++ react-practice-for-exercise/src/components/star.jsx
@@ -7,11 +7,18 @@ import filledStar from '../assets/fill star.jpg'
 
 export default function Star(props){
     const starIcon = props.isFilled ? filledStar : blankStar
+    const buttonLabel = props.isFilled ? "Unmark as favorite" : "Mark as favorite"
     return (
-        <img 
-                        src={starIcon} 
-                        className="card--favorite"
-                        onClick={props.handleClick}
-                    />
+        <button onClick={props.handleClick} 
+            aria-label={buttonLabel}
+            aria-pressed={props.isFilled}
+            className="card--favorite-button"
+        >
+            <img 
+                src={starIcon} 
+                className="card--favorite"
+                
+            />
+        </button>
     )
 }
