7757ae7f95fcc3251501a480651fc0982fb87e55
Anita Mishra <anitahmishra@gmail.com>
2024-05-11 20:16:36 +0530

    box challange react

--- react-practice-for-exercise/src/components/boxChallanges.jsx
+++ react-practice-for-exercise/src/components/boxChallanges.jsx
@@ -2,15 +2,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import boxes from "./boxes";
+import Box from "./box";
 
 export default function Boxchallanges(props){
     const [sqaures, setSquares] = React.useState(boxes)
 
-    const styles = {
-        backgroundColor : props.darkMode ? "#222222" : "#cccccc"
-    }
+  
     const sqauresElements = sqaures.map(square =>(
-        <div style={styles} className="box" key={square.id}></div>
+        <Box key={square.id} on={square.on}/>
     ))
     return (
         <main>
