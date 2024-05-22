ef34d6a26995164a85bd9d1c4271a0d4808969ab
Anita Mishra <anitahmishra@gmail.com>
2024-05-22 18:01:36 +0530

    map array in state

--- react-practice-for-exercise/src/components/boxChallanges.jsx
+++ react-practice-for-exercise/src/components/boxChallanges.jsx
@@ -7,9 +7,41 @@ import Box from "./box";
 export default function Boxchallanges(props){
     const [sqaures, setSquares] = React.useState(boxes)
 
+    
+    function toggle(id){
+        // second mehod
+        // setSquares(prevSquares => {
+        //     const newSquares = []
+        //     for(let i=0; i<prevSquares.length; i++){
+        //         const currentSquare = prevSquares[i]
+        //         if(currentSquare.id === id) {
+        //             const updateSquares = {
+        //                 ...currentSquare,
+        //                 on: !currentSquare.on
+        //             }
+        //             newSquares.push(updateSquares)
+
+        //         }else {
+        //             newSquares.push(currentSquare)
+        //         }
+        //     }
+        //     return newSquares
+        // })
+
+        setSquares(prevSquares => {
+            return prevSquares.map(square => {
+                return square.id === id ? {...square, on: !square.on} : square
+            })
+        })
+    
+    }
   
     const sqauresElements = sqaures.map(square =>(
-        <Box key={square.id} on={square.on}/>
+        <Box 
+        key={square.id} 
+        id={square.id}
+        on={square.on}
+        toggle={toggle}/>
     ))
     return (
         <main>
