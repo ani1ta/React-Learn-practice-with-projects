dda80a3e1d7039e3682d2a1db2315fef8b5e7c4e
Anita Mishra <anitahmishra@gmail.com>
2024-05-25 20:27:35 +0530

    Conditional rendering practice

--- react-practice-for-exercise/src/components/conditional rendering practice.jsx
+++ react-practice-for-exercise/src/components/conditional rendering practice.jsx
@@ -6,9 +6,19 @@ export default function ConditionalRenderingPractice(){
 
     return (
         <div >
-            {
+            {/* {
                 messages.length > 0 && 
                 <h1>You have {messages.length} unread messages</h1>
+            } */}
+
+
+            {
+                messages.length === 0 ?
+                // eslint-disable-next-line react/no-unescaped-entities
+                <h1>You're all caught up </h1> :
+                <h1>
+                    You have {messages.length} unread {messages.length > 1 ? "messages" : "message"} 
+                </h1>
             }
         </div>
     )
