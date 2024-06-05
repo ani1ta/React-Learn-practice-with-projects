82e1f0ac0800a922e8ab4fc07426bfbcba687138
Anita Mishra <anitahmishra@gmail.com>
2024-06-05 16:58:46 +0530

    Meme generator useEffect fetch API

--- Meme-generator/src/components/Meme.jsx
+++ Meme-generator/src/components/Meme.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-debugger */
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import memesData from './memesData'
+// import memesData from './memesData'
 export default function Meme(){
     
 
@@ -22,19 +22,41 @@ export default function Meme(){
      * correct way.
      */
     
+     /**
+     * Challenge: 
+     * As soon as the Meme component loads the first time,
+     * make an API call to "https://api.imgflip.com/get_memes".
+     * 
+     * When the data comes in, save just the memes array part
+     * of that data to the `allMemes` state
+     * 
+     * Think about if there are any dependencies that, if they
+     * changed, you'd want to cause to re-run this function.
+     * 
+     * Hint: for now, don't try to use an async/await function.
+     * Instead, use `.then()` blocks to resolve the promises
+     * from using `fetch`. We'll learn why after this challenge.
+     */
+    
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText : "",
         randomImage : "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemeImages, setAllMemeImages] = React.useState([])
     
+    React.useEffect(()=> {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemeImages(data.data.memes))
+    }, [])
+
     function handlememeButton(){
         // console.log("clicked")
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        // const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+        const url = allMemeImages[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage : url
