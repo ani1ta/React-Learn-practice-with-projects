806fe7fe4eb54e4d39bd7fee26df9b26c7be55ef
Anita Mishra <anitahmishra@gmail.com>
2024-06-05 13:20:03 +0530

    UseEffect react

--- react-practice-for-exercise/src/useEffect React/MakingApiCall.jsx
+++ react-practice-for-exercise/src/useEffect React/MakingApiCall.jsx
@@ -3,17 +3,31 @@ import React from "react";
 
 export default function MakingApiCall(){
     const [starWarData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
 
     console.log("component rederer")
 
-    fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => console.log(data))
+    // fetch("https://swapi.dev/api/people/1")
+    //     .then(res => res.json())
+    //     .then(data => setStarWarsData(data))
+
+
+    // side effect
+
+
+    React.useEffect(function (){
+        console.log("Effect ran")
+        // fetch("https://swapi.dev/api/people/1")
+        // .then(res => res.json())
+        // .then(data => setStarWarsData(data))
+    }, [count])
 
 
     return(
         <div>
             <pre>{JSON.stringify(starWarData, null, 2)}</pre>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>ADD</button>
         </div>
     )
 }
